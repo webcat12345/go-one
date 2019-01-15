9742a485707a0d57091d9cae00ac6f20463f0a28
webcat12345 <webcat91@gmail.com>
2019-01-15 00:35:13 -0800

    go-echo tutorial phase 1 complete

---
+++ server.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"io"
+	"net/http"
+	"os"
+)
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+func main() {
+	e := echo.New()
+
+	// root level middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// group level middleware
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username string, password string, context echo.Context) (b bool, e error) {
+		if username == "joe" && password == "secret" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	// route level middleware
+	track := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(context echo.Context) error {
+			println("request to /users")
+			return next(context)
+		}
+	}
+
+	// home routing
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	// params - middleware is used
+	e.GET("/users/:id", getUser, track)
+	// form data
+	e.POST("/save", save)
+	// file save
+	e.POST("/save_photo", savePhoto)
+	// query params
+	e.GET("/show", show)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func save(c echo.Context) error {
+	// using the struct for json parse
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, u)
+}
+
+func savePhoto(c echo.Context) error {
+	name := c.FormValue("name")
+	avatar, err := c.FormFile("avatar")
+
+	if err != nil {
+		return err
+	}
+
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
+
+}
+
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team: "+team+", member: "+member)
+}
